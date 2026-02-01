f3869f8179e6cc7687bcd72023ac377cf1f7de49
elmarkrueger <107558687+elmarkrueger@users.noreply.github.com>
2026-02-01 12:39:51 +0100

    Add AnimagineXL4_Creature_Styler to NODE_CLASS_MAPPINGS and NODE_DISPLAY_NAME_MAPPINGS

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .animagine_v4 import (AnimagineXL4_Character_Factory,
+                           AnimagineXL4_Creature_Styler,
                            AnimagineXL4_Prompt_Manager,
                            AnimagineXL4_Prompt_Styler)
 
@@ -6,12 +7,14 @@ NODE_CLASS_MAPPINGS = {
     "AnimagineXL4_Prompt_Styler": AnimagineXL4_Prompt_Styler,
     "AnimagineXL4_Prompt_Manager": AnimagineXL4_Prompt_Manager,
     "AnimagineXL4_Character_Factory": AnimagineXL4_Character_Factory,
+    "AnimagineXL4_Creature_Styler": AnimagineXL4_Creature_Styler,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "AnimagineXL4_Prompt_Styler": "Animagine XL 4.0 Prompt Styler (Standard)",
     "AnimagineXL4_Prompt_Manager": "Animagine XL 4.0 Prompt Manager (Simple)",
     "AnimagineXL4_Character_Factory": "Animagine XL 4.0 Character Factory (Expert)",
+    "AnimagineXL4_Creature_Styler": "Animagine XL 4.0 Creature Styler (Cyborg/Robot/Monster)",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
